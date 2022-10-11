874bd298acd6f2ac1226854efb34ee1536a81a61
matth747 <109696245+matth747@users.noreply.github.com>
2022-10-11 12:38:22 -0500

    set up geo code api to find cordinates for a given city.  Then used weather api to log the weather for the given cordinates

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,44 @@
 
 
-    $(".btn").click(function(){
+    $(".btn-primary").click(function(){
         
-        var APIUrl =  "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + APIKey;
-
         var APIKey = "&appid=cba8af4433e354992fc4db0cecdab63f"
-        var cityInput= $("#whatsInTheBox").val();
+        var cityInput= $("#textBox").val();
+        var APIUrl =  "https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + APIKey;
         
-        fetch(APIUrl)
+        fetch(APIUrl) 
+
             .then(function (response) {
                 return response.json();
+            
         })
             .then(function (data) {
-            console.log(data)
-    });
+                console.log(data)
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+                console.log(lat)
+                console.log(lon)
+        
+                var cordUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + APIKey
 
+        
+                console.log(cordUrl)
+                fetch(cordUrl)
+        
+                .then(function (response) {
+                    return response.json();
+                })
+        
+                .then(function (cordData) {
+                    console.log(cordData)
+                })
+        
+        
+               $("#textBox").val("");
 
-       $("#whatsInTheBox").val("");
+            })
+    
     });
     
-
-
-
- 
+   
+   
